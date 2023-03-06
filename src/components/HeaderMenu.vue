1cba07550e0cc797dc0be9cc55d5f32d08529ae6
LX <849553682@qq.com>
2023-03-06 23:57:11 +0800

    菜单修改

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="menu">
-    <div>首页</div>
-    <div>解决方案</div>
-    <div>关于我们</div>
-    <div>加入我们</div>
-    <div>联系我们</div>
+    <div v-for="item of menus" :key="item.key" :class="{ active: item.key == activePage }" @click="changePage(item)">{{
+      item.name }}</div>
   </div>
 </template>
-<script setup lang="ts" ></script>
+<script setup lang="ts" >
+import { useRouter } from 'vue-router'
+import { ref, reactive } from 'vue';
+const router = useRouter()
+let activePage = ref('HomePage');
+
+let menus = reactive([
+  { name: '首页', key: 'HomePage', router: '/' },
+  { name: '解决方案', key: 'SolutionPage', router: '/solution' },
+  { name: '关于我们', key: 'AboutUsPage', router: '/aboutUs' },
+  { name: '加入我们', key: 'JoinUsPage', router: '/joinUs' },
+  { name: '联系我们', key: 'LinkUsPage', router: '/linkUs' }
+])
+
+let changePage = (item: any) => {
+  activePage.value = item.key;
+  router.push(item.router);
+}
+</script>
 <style lang="scss" scoped>
 .menu {
   display: flex;
@@ -19,5 +34,12 @@
   font-weight: 400;
   padding-right: 10%;
   font-family: 联盟锐黑;
+  color: rgba(153, 153, 153);
+  div {
+    cursor: pointer;
+  }
+  .active {
+    color: black;
+  }
 }
 </style>
